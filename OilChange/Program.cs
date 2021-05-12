c160d59c8e884b7dc20f2fed7b1943cdab68696e
Jason Straub <jstraub@my365.bellevue.edu>
2021-05-11 19:03:28 -0500

    Set up the vehicle and oil classes.

--- OilChange/Program.cs
+++ OilChange/Program.cs
@@ -6,6 +6,35 @@ using System.Windows.Forms;
 
 namespace OilChange
 {
+    public class Vehicle {
+        string make;
+        string model;
+        int year;
+        Vehicle( string inmake, string inmodel, int inyear )
+        {
+            make = inmake;
+            model = inmodel;
+            year = inyear;
+        }
+    }
+    public class Oil
+    {
+        string oilweight;
+        string oilbrand;
+        double quantity;
+        double oilprice;
+        string filterbrand;
+        double filterprice;
+        Oil(string weight, string obrand, double quant, double oprice, string fbrand, double fprice)
+        {
+            oilweight = weight;
+            oilbrand = obrand;
+            quantity = quant;
+            oilprice = oprice;
+            filterbrand = fbrand;
+            filterprice = fprice;
+        }
+    }
     static class Program
     {
         /// <summary>
