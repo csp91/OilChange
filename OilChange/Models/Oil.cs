d522ba10763370ae6976f7226860171fd53e1b62
csp91 <csp91@github.com>
2021-05-16 18:54:03 -0500

    Change file structure

---
+++ OilChange/Models/Oil.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OilChange
+{
+    public class Oil
+    {
+        string oilweight;
+        string oilbrand;
+        double quantity;
+        double oilprice;
+        string filterbrand;
+        double filterprice;
+        
+        public Oil(string weight, string obrand, double quant, double oprice, string fbrand, double fprice)
+        {
+            oilweight = weight;
+            oilbrand = obrand;
+            quantity = quant;
+            oilprice = oprice;
+            filterbrand = fbrand;
+            filterprice = fprice;
+        }
+    }
+}
---
+++ OilChange/Models/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OilChange
+
+{
+    public class Vehicle
+    {
+        string make;
+        string model;
+        int year;
+
+        public Vehicle(string inmake, string inmodel, int inyear)
+        {
+            make = inmake;
+            model = inmodel;
+            year = inyear;
+        }
+    }
+}
